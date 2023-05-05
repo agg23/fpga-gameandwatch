e0dece25896ca1d171bbb529fb5df541f99a515c
Adam Gastineau <adam@agg.im>
2023-05-05 12:04:46 -0700

    Initialized Gateware project

---
+++ rtl/gameandwatch.sv
@@ -0,0 +1,11 @@
+//------------------------------------------------------------------------------
+// SPDX-License-Identifier: MIT
+// SPDX-FileType: SOURCE
+// SPDX-FileCopyrightText: (c) 2023, agg23
+//------------------------------------------------------------------------------
+
+`default_nettype none
+
+module gameandwatch ();
+
+endmodule
